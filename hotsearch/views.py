8fa6694cb28e003b290380c5599717d04d666957
Name <213232464@seu.edu.cn>
2025-03-11 19:21:21 +0800

    完成热搜在网页的部署

--- hotsearch/views.py
+++ hotsearch/views.py
@@ -5,5 +5,12 @@ from django.shortcuts import render
 from hotsearch.models import HotSearch
 
 def hot_search_list(request):
-    hot_searches = HotSearch.objects.all().order_by('-created_at')  # 按创建时间降序排列
-    return render(request, 'hotsearch/hot_search_list.html', {'hot_searches': hot_searches})
+    """
+    展示微博热搜榜的视图
+    """
+    # 从数据库中获取所有热搜数据
+    hot_searches = HotSearch.objects.all().order_by('id')  # id前加-决定倒序（按照最新排序）
+    context = {
+        'hot_searches': hot_searches
+    }
+    return render(request, 'hotsearch/hot_search_list.html', context)
